1ba8019c4a24ba06b87c7a4e61f9f6c3aa921ebd
Jim Ramsay <i.am@jimramsay.com>
2016-05-18 14:55:34 -0400

    Add plumbing to create new cards via TrelloAPIUtil
    This requires that we begin asking for write access when logging in.
    
    Signed-off-by: Jim Ramsay <i.am@jimramsay.com>

--- app/src/main/kotlin/com/github/oryanmat/trellowidget/activity/AddCardActivity.kt
+++ app/src/main/kotlin/com/github/oryanmat/trellowidget/activity/AddCardActivity.kt
@@ -8,13 +8,19 @@ import com.github.oryanmat.trellowidget.R
 
 import android.appwidget.AppWidgetManager.EXTRA_APPWIDGET_ID
 import android.appwidget.AppWidgetManager.INVALID_APPWIDGET_ID
+import android.widget.Toast
+import com.android.volley.Response
+import com.android.volley.VolleyError
 import com.github.oryanmat.trellowidget.T_WIDGET
-import com.github.oryanmat.trellowidget.util.getBoard
-import com.github.oryanmat.trellowidget.util.getList
+import com.github.oryanmat.trellowidget.model.NewCard
+import com.github.oryanmat.trellowidget.util.*
 import kotlinx.android.synthetic.main.activity_add_card.*
+import java.nio.charset.Charset
 
 class AddCardActivity : Activity() {
     internal var appWidgetId = INVALID_APPWIDGET_ID
+    internal var cardsAdded = 0
+    internal var addSingleCard = false
 
     enum class Location {
         INSERT_AT_TOP,
@@ -37,9 +43,11 @@ class AddCardActivity : Activity() {
         val board = getBoard(appWidgetId)
 
         add_card_board_name.text = "${board.name} / ${list.name}"
+
         addCloseButton.setOnClickListener { v -> close() }
         topButton.setOnClickListener { v -> addNewCard(Location.INSERT_AT_TOP) }
         bottomButton.setOnClickListener { v -> addNewCard(Location.INSERT_AT_BOTTOM) }
+        setButtonsEnabled(true)
     }
 
     fun addNewCard(where: Location) {
@@ -47,18 +55,60 @@ class AddCardActivity : Activity() {
         if (newTitle.isEmpty()) {
             return
         }
-        add_card_title.setText("")
+        setButtonsEnabled(false)
 
         val list = getList(appWidgetId)
         val board = getBoard(appWidgetId)
-        // TODO: Actually do the add
 
-        Log.i(T_WIDGET, "Would add new card to " + (if (where == Location.INSERT_AT_TOP) "top" else "bottom") + " of ${board.name}/${list.name}: $newTitle")
+        val newCard = NewCard(list.id, newTitle, when(where) {
+          Location.INSERT_AT_TOP -> NewCard.POS_TOP
+          Location.INSERT_AT_BOTTOM -> NewCard.POS_BOTTOM
+        })
 
-        // TODO: Optionally finish(); if the user only wants to add one card
+        val description = "${board.name} / ${list.name}"
+        Log.d(T_WIDGET, "Adding new card to ${if (where == Location.INSERT_AT_TOP) "top" else "bottom"} of $description: $newTitle")
+        val listener = AddCardListener(description)
+        TrelloAPIUtil.instance.addNewCard(newCard, listener)
+        // TODO: Start a spinner or something?
     }
 
-    fun close() {
-        finish() // TODO: Send a widget refresh broadcast
+    inner class AddCardListener(val description: String) : Response.Listener<String>, Response.ErrorListener {
+        override fun onResponse(response: String) {
+            // TODO: The 'response' is the json of the newly-created card - We could maybe inject this into the RemoteView without forcing a refresh?
+            Log.i(T_WIDGET, "Added card to $description")
+            cardsAdded++
+            if (addSingleCard)
+                close()
+            else
+                resetInput()
+            Toast.makeText(this@AddCardActivity, getString(R.string.add_card_success), Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onErrorResponse(error: VolleyError) {
+            Log.e(T_WIDGET, "Add Card failed: ${error.networkResponse.data.toString(Charset.defaultCharset())}", error)
+            val message = getString(when(error.networkResponse.statusCode) {
+                401 -> R.string.add_card_permission_failure
+                else -> R.string.add_card_failure
+            })
+            Toast.makeText(this@AddCardActivity, message, Toast.LENGTH_LONG).show()
+            setButtonsEnabled(true)
+        }
+    }
+
+    private fun resetInput() {
+        add_card_title.setText("")
+        setButtonsEnabled(true)
+    }
+
+    private fun setButtonsEnabled(enabled: Boolean) {
+        for (button in arrayOf(addCloseButton, topButton, bottomButton))
+            button.isEnabled = enabled
+    }
+
+    private fun close() {
+        finish()
+        if (cardsAdded > 0) {
+            sendBroadcast(createRefreshIntent(appWidgetId))
+        }
     }
 }
